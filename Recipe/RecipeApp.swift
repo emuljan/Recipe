f727039c70a1eb0434b0e832f4815c3232e70e28
emuljan <emul.hayde@gmail.com>
2025-02-11 14:43:27 -0800

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Emma Babayan on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeTests/RecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Emma Babayan on 2/11/25.
+//
+
+import Testing
+@testable import Recipe
+
+struct RecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
