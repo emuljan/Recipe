99585606b2104d4d323d0bcb026802f030dd1fc6
emuljan <emul.hayde@gmail.com>
2025-02-11 14:47:19 -0800

    Adding Recipe Model

---
+++ Recipe/Models/Recipes.swift
@@ -0,0 +1,20 @@
+//
+//  Recipes.swift
+//  Recipe
+//
+//  Created by Emma Babayan on 2/11/25.
+//
+
+struct RecipesResponse {
+    let recipes: [Recipe]
+}
+
+struct Recipe {
+    let cuisine: String
+    let name: String
+    let photoLargeUrl: String?
+    let photoSmallUrl: String?
+    let uuid: String
+    let sourceUrl: String?
+    let youtubeUrl: String?
+}
